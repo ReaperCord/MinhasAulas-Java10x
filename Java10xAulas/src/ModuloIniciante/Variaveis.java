76d82f61d451555f0e2e6572bf93d4b3f472e89a
Leandro R. <36233004+ReaperCord@users.noreply.github.com>
2024-12-31 01:54:37 -0300

    commit inicial

---
+++ Java10xAulas/src/ModuloIniciante/Variaveis.java
@@ -0,0 +1,13 @@
+package ModuloIniciante;
+/*
+    * Tipos de dados
+    *
+*/
+
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome = "Naurto";
+        int idade = 18;
+        boolean vivo = true;
+    }
+}
---
+++ JavaNinja/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,19 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+            Dados primitivos: int, double, float, char, boolean, short ...
+            Objetivo: criar um ninja
+         */
+
+        int idade = 16; //valor máximo: 2147483647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 99999L; //Valor máximo: 3 trilhões e caralhada
+        
+
+
+    }
+}
