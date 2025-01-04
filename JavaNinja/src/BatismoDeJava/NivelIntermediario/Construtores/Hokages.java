4ff346f667b8922ba1ff370fe67b3237eb075b37
Leandro R. <36233004+ReaperCord@users.noreply.github.com>
2025-01-04 03:59:59 -0300

    POO - construtores

--- JavaNinja/src/BatismoDeJava/NivelIntermediario/Construtores/Hokages.java
+++ JavaNinja/src/BatismoDeJava/NivelIntermediario/Construtores/Hokages.java
@@ -1,4 +1,27 @@
 package BatismoDeJava.NivelIntermediario.Construtores;
 
 public class Hokages {
+
+    String nome;
+    String aldeia;
+    double altura;
+    int idade;
+    boolean vivoMorto;
+
+
+    //all agrs constructor
+    /*public Hokages(String nome, int idade, boolean vivoMorto) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoMorto = vivoMorto;
+    }*/
+
+    //da pra criar um construtor automaticamente com o alt + ins ex:
+    public Hokages(String nome, String aldeia, double altura, int idade, boolean vivoMorto) {
+        this.nome = nome;
+        this.aldeia = aldeia;
+        this.altura = altura;
+        this.idade = idade;
+        this.vivoMorto = vivoMorto;
+    }
 }
--- JavaNinja/src/BatismoDeJava/NivelIntermediario/Construtores/Main.java
+++ JavaNinja/src/BatismoDeJava/NivelIntermediario/Construtores/Main.java
@@ -1,4 +1,17 @@
 package BatismoDeJava.NivelIntermediario.Construtores;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        //no args constructor
+        /*Hokages Hashirama = new Hokages();
+        Hashirama.nome = "Hashirama";
+        Hashirama.idade = 74;
+        Hashirama.vivoMorto = false;*/
+
+        //all args constructor
+        Hokages Tobirama = new Hokages("Tobirama Senju", "Aldeia da Folha", 1.80, 45, false);
+        System.out.println(Tobirama.nome + "\n" + Tobirama.aldeia + "\n" + Tobirama.idade + "\n" + Tobirama.altura + "\n" + Tobirama.vivoMorto + "\n");
+    }
 }
