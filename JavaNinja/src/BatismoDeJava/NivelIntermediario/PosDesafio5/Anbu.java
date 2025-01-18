b2b9b80c9126087f64d071858a2f8ab262cddb1d
Leandro R. <36233004+ReaperCord@users.noreply.github.com>
2025-01-17 22:21:36 -0300

    Final class

---
+++ JavaNinja/src/BatismoDeJava/NivelIntermediario/PosDesafio5/Anbu.java
@@ -0,0 +1,13 @@
+package BatismoDeJava.NivelIntermediario.PosDesafio5;
+/*
+Classes Final são classes aonde não queremos que nenhuma outra classe herde
+seus atributos, podemos gerar objetos apartir mas a mesma não pode ter classes filhas
+*/
+
+
+final public class Anbu {
+
+    public void Anbu () {
+        System.out.println("Eu sou um Anbu, um dos melhores entre os melhores. Os ninjas de elite da vila! A rota de Konoha");
+    }
+}
